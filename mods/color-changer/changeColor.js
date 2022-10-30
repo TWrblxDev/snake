298da090b0d5a71524a6778d152880e8a206cf09
TWrblxDev <64037997+TWrblxDev@users.noreply.github.com>
2022-10-30 18:27:58 -0400

    Update changeColor.js

--- mods/color-changer/changeColor.js
+++ mods/color-changer/changeColor.js
@@ -2,7 +2,7 @@
 
 /* Dark mode */
 var Mod = () => {
-    colors.walls =      '#000000';
+    colors.walls =      '#111111';
     colors.shadows =    '#000000';
     colors.lightTiles = '#353536';
     colors.darkTiles =  '#2b2b2b';
