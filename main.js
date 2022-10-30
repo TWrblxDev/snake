4c660140ed6892ad9ce416fb4f44edcb2472a861
TWrblxDev <64037997+TWrblxDev@users.noreply.github.com>
2022-10-30 18:30:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -258,5 +258,6 @@ modsPaths.forEach(mod => {
         });
         
         console.log('Loaded mod ' + json.name);
+        console.log('[DEBUG] Mod name' + json.name 'Has been loaded and is ready.};
     });
 });
